9e274bd665b5cbd8637f143edc006ed6117d032b
Douglas Lowder <dlowder@salesforce.com>
2019-02-24 09:55:03 -0800

    Use oauth react-native-force for authentication

--- components/Login.js
+++ components/Login.js
@@ -1,61 +1,17 @@
-import React, { PropTypes } from 'react';
-import { WebView } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import ReactNativeForce from '../ReactNativeForce';
 
-import Config from '../Config';
+export default class Login extends Component<{
+  onSuccess: Function
+}> {
+  componentDidMount() {
+    ReactNativeForce.oauth.authenticate().then(creds => {
+      this.props.onSuccess(creds.accessToken, creds.instanceUrl);
+    });
+  }
 
-// Component that displays the OAuth login page and saves
-// the resulting access token and instance URL to the provided onSuccess hook.
-const Login = ({ onSuccess }) => {
-  let doOnLoadStart = state => {
-    let startsWithRedirect = state.nativeEvent.url.startsWith(
-      Config.oauthRedirect
-    );
-    let accessIndex = state.nativeEvent.url.indexOf('access_token');
-
-    if (startsWithRedirect && accessIndex >= 0) {
-      // If the browser is being redirected to the OAuth redirect URL,
-      // then extract the access token and instance URL from the query
-      // string and signal that login has succeeded.
-      let decodedUrl = decodeURIComponent(state.nativeEvent.url);
-      let getParam = (source, item) => {
-        let itemIndex = source.indexOf(item);
-        if (itemIndex >= 0) {
-          let indexPlus = source.substr(itemIndex);
-          let ampIndex = indexPlus.indexOf('&');
-          let startIndex = item.length + 1;
-          let strLength = ampIndex - startIndex;
-          let result = indexPlus.substr(startIndex, strLength);
-          return result;
-        } else {
-          return '';
-        }
-      };
-
-      let accessToken = getParam(decodedUrl, 'access_token');
-      let instanceUrl = getParam(decodedUrl, 'instance_url');
-      onSuccess(accessToken, instanceUrl);
-    }
-  };
-
-  let uri =
-    Config.loginUrl +
-    'display=popup&response_type=token' +
-    '&client_id=' +
-    encodeURIComponent(Config.consumerKey) +
-    '&redirect_uri=' +
-    encodeURIComponent(Config.oauthRedirect);
-
-  return (
-    <WebView
-      onLoadStart={doOnLoadStart}
-      source={{ uri }}
-      style={{ marginTop: 20 }}
-    />
-  );
-};
-
-Login.propTypes = {
-  onSuccess: PropTypes.func.isRequired
-};
-
-export default Login;
+  render() {
+    return <View />;
+  }
+}
